679e2f417cf7d0c55085ff00aa780e54fc7e99c9
brendoprendo <brendan.prednis@gmail.com>
2021-05-14 22:18:25 -0700

    first commit yee: just some outlining and thoughts on what is necessary. Index.html. index.tsx added.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Micro React-Typescript Project</h1>
+      <h2>Remember to change your title in the html :)</h2>
+    </div>
+  )
+}
