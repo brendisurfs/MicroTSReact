b113e6105f635b3ea1a8902e58df825ec87c6714
brendoprendo <brendan.prednis@gmail.com>
2021-05-14 22:43:08 -0700

    added README.md

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export const App: React.FC = (): JSX.Element => {
   return (
     <div>
-      <h1>Micro React-Typescript Project</h1>
-      <h2>Remember to change your title in the html :)</h2>
+      <h2>
+        Remember to change your title in the html. Have fun dude! -Brendan
+      </h2>
     </div>
   )
 }
